37747b3ba316fc3a31edf210972d09e4a427645f
hinriksnaer <hinriksnaer@gmail.com>
2020-03-01 22:16:07 +0000

    added query search

--- HW2/word2vec_processing.py
+++ HW2/word2vec_processing.py
@@ -15,22 +15,42 @@ def doc2vector(doc, embedding_dict):
     return np.mean(stack, axis=0)
 
 def get_embedding_dict(path):
-    embedding = pd.read_csv('path', sep=" ", header=None, index_col=0)
-    return embedding
+    embedding = pd.read_csv(path, sep=" ", header=None, index_col=0)
+    embedding_dict = embedding.T.to_dict('list')
+    return embedding_dict
 
 def get_word_embedding(path, word):
-    embedding = pd.read_csv('path', sep=" ", header=None, index_col=0)
+    embedding = pd.read_csv(path, sep=" ", header=None, index_col=0)
+    embedding = embedding.T.to_dict('list')
     return embedding[word]
 
 def cosine_similarity(a,b):
-    return dot(a, b)/(norm(a)*norm(b))
+    return np.dot(a, b)/(np.linalg.norm(a)*np.linalg.norm(b))
 
+def find_n_similar_words(word, n, path):
+    word = read_ap.process_text(word)    
+    word = word[0]
+    result = []
+    emb_dict = get_embedding_dict(path)
+    for idx, value in enumerate(emb_dict):
+        similarity = cosine_similarity(emb_dict[word], emb_dict[value])
+        result.append((value, similarity))
+    result.sort(key=lambda tup: tup[1], reverse=True)
+    return result[0:n]
 
-#embedding = pd.read_csv('word_vectors.csv', sep=" ", header=None, index_col=0)
-#
-#print(embedding)
-#embedding_dict = embedding.T.to_dict('list')
-#docs = read_ap.get_processed_docs()
-#
-#vect = doc2vector(docs['AP891231-0014'], embedding_dict)
-#print('hello')
+def find_n_relevant_docs(query, docs, n, path):
+    doc_vectors = []
+    query = read_ap.process_text(query)
+
+    embedding_dict = get_embedding_dict(path)
+    query_vector = doc2vector(query, embedding_dict)
+    for key in docs:
+        doc_vector = doc2vector(docs[key], embedding_dict)
+        doc_vectors.append((key, doc_vector))
+    
+    similaries = []
+    for vector in doc_vectors:
+        similarity = cosine_similarity(vector[1], query_vector)
+        similaries.append((vector[0], similarity))
+    similaries.sort(key=lambda tub: tub[1], reverse=True)
+    return similaries[0:n]
