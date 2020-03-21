f4ccabac17d00046f254fd6148a6a4cd79cd5a21
Aunel <christiaan_vlist@hotmail.com>
2020-03-21 22:17:48 +0100

    Implemented nDCG for listwise

--- HW3/listwise.py
+++ HW3/listwise.py
@@ -13,149 +13,162 @@ import ranking as rnk
 
 # Time functions
 import time
+from datetime import datetime as time
 
 # Evaluate model import
 from pointwise_evaluation import evaluate_model
 
 def run_epoch(model, optimizer, data, eval_every=2500, sigma=1, IRM='err'):
     
-    # Parameters
-    overall_loss = 0
-    epoch_loss = 0
-    
-    # Main Pairwise RankNet function
-    for i, qid in enumerate(np.arange(data.train.num_queries())):
+	# Parameters
+	overall_loss = 0
+	epoch_loss = 0
 
-        # Zero the gradient buffer and get doc,query combinations, labels and scores
-        optimizer.zero_grad()
-        qd_feats = data.train.query_feat(qid)
-        qd_labels = data.train.query_labels(qid)
-        scores = model.forward(torch.tensor(qd_feats).float().cuda())
-        
-        # Make em torchy?
-        qd_feats = torch.tensor(qd_feats).cuda()
-        qd_labels = torch.tensor(qd_labels).cuda()
+	# Main Pairwise RankNet function
+	for i, qid in enumerate(np.arange(data.train.num_queries())):
 
-        # TODO: Fix this spaghetti shit
-        scores_d = scores.detach()
-        ranking_boi = np.float64(scores.squeeze().cpu().detach().numpy())
-        
-        # Get the ranking 
-        if not isinstance(ranking_boi, np.ndarray):
-            ranking_boi = np.array([ranking_boi])
-        ranking, inv_ranking = rnk.rank_and_invert(ranking_boi)
+		# Zero the gradient buffer and get doc,query combinations, labels and scores
+		optimizer.zero_grad()
+		qd_feats = data.train.query_feat(qid)
+		qd_labels = data.train.query_labels(qid)
+		scores = model.forward(torch.tensor(qd_feats).float().cuda())
+		
+		# Make em torchy?
+		qd_feats = torch.tensor(qd_feats).cuda()
+		qd_labels = torch.tensor(qd_labels).cuda()
 
-        ranking = torch.tensor(ranking.copy()).cuda()
+		# TODO: Fix this spaghetti shit
+		scores_d = scores.detach()
+		ranking_boi = np.float64(scores.squeeze().cpu().detach().numpy())
+		
+		# Get the ranking 
+		if not isinstance(ranking_boi, np.ndarray):
+			ranking_boi = np.array([ranking_boi])
+		ranking, inv_ranking = rnk.rank_and_invert(ranking_boi)
 
-        # Get rid of pesky 1-document queries and initialize loss
-        if len(scores) < 2:
-            continue
-        loss = 0
+		ranking = torch.tensor(ranking.copy()).cuda()
 
-        # Vectorize the loss calculation
-        # Calculate all score differences
-        scorediff = scores_d - scores_d.T
+		# Get rid of pesky 1-document queries and initialize loss
+		if len(scores) < 2:
+			continue
+		loss = 0
 
-        # Calculate all signs
-        squeeze_labels = qd_labels.unsqueeze(-1)
-        signs = torch.sign(squeeze_labels - qd_labels).float()
+		# Vectorize the loss calculation
+		# Calculate all score differences
+		scorediff = scores_d - scores_d.T
 
-        # Loss is just vectorized formula
-        lambdas_ij = sigma * ((1 / 2) * (1 - scorediff) - (1 / (1 + torch.exp(sigma * scorediff))))
+		# Calculate all signs
+		squeeze_labels = qd_labels.unsqueeze(-1)
+		signs = torch.sign(squeeze_labels - qd_labels).float()
 
-        # TODO: Calc IRM
-        np_labels = qd_labels.cpu().numpy()
-        np_ranking = ranking.cpu().numpy()
-        pred_perms = []
-        sorted_pred = np.array([np_labels[idx] for idx in np_ranking])
-        pred_perms.append(sorted_pred)
+		# Loss is just vectorized formula
+		lambdas_ij = sigma * ((1 / 2) * (1 - scorediff) - (1 / (1 + torch.exp(sigma * scorediff))))
 
-        for p in range(len(sorted_pred)):
-            for q in range(p+1, len(sorted_pred)):
-                perm_pred = sorted_pred.copy()
-                temp = sorted_pred[p] 
-                perm_pred[p] = sorted_pred[q]
-                perm_pred[q] = temp
-                pred_perms.append(perm_pred)
-        pred_perms = np.array(pred_perms)
+		# TODO: Calc IRM
+		np_labels = qd_labels.cpu().numpy()
+		np_ranking = ranking.cpu().numpy()
+		pred_perms = []
+		sorted_pred = np.array([np_labels[idx] for idx in np_ranking])
+		pred_perms.append(sorted_pred)
 
-        if(IRM == 'err'):
-            ranking_measure = err(pred_perms)
-        elif(IRM == 'ndcg'):
-            ranking_measure = 0
+		for p in range(len(sorted_pred)):
+			for q in range(p+1, len(sorted_pred)):
+				perm_pred = sorted_pred.copy()
+				temp = sorted_pred[p] 
+				perm_pred[p] = sorted_pred[q]
+				perm_pred[q] = temp
+				pred_perms.append(perm_pred)
+		pred_perms = np.array(pred_perms)
 
-        deltas = np.abs(ranking_measure[0] - ranking_measure[1:])
-        delta_irm = np.zeros((len(ranking), len(ranking)))
-        delta_irm[np.triu_indices(len(ranking), 1)] = deltas
-        delta_irm = torch.from_numpy(delta_irm + delta_irm.T).float().cuda()
+		if(IRM == 'err'):
+			ranking_measure = err(pred_perms)
+		elif(IRM == 'ndcg'):
+			label_rank = np.sort(np_labels)[::-1]
+			ranking_measure = ndcg(pred_perms,label_rank)
 
-        lambdas_ij = lambdas_ij * delta_irm
-        lambas_i = lambdas_ij.sum(dim=1)
-        loss = scores.squeeze() * lambas_i
-        loss = loss.sum()
+		deltas = np.abs(ranking_measure[0] - ranking_measure[1:])
+		delta_irm = np.zeros((len(ranking), len(ranking)))
+		delta_irm[np.triu_indices(len(ranking), 1)] = deltas
+		delta_irm = torch.from_numpy(delta_irm + delta_irm.T).float().cuda()
 
-        # Keep track of rolling average
-        overall_loss += loss / (len(ranking) ** 2)
+		lambdas_ij = lambdas_ij * delta_irm
+		lambas_i = lambdas_ij.sum(dim=1)
+		loss = scores.squeeze() * lambas_i
+		loss = loss.sum()
 
-        if (i+1) % eval_every == 0:
-            avg_ndcg = evaluate_model(model, data.validation,regression=True)
-            print("NCDG: ", avg_ndcg)
+		# Keep track of rolling average
+		overall_loss += loss / (len(ranking) ** 2)
 
-        # Update gradients
-        loss.backward()
-        optimizer.step()
+		if (i+1) % eval_every == 0:
+			avg_ndcg = evaluate_model(model, data.validation,regression=True)
+			print("NCDG: ", avg_ndcg)
 
-        #break
-    
-    #
-    #print(ranking)
+		# Update gradients
+		loss.backward()
+		optimizer.step()
 
-    print(ranking)
-    print("epoch_loss: ", overall_loss / data.train.num_queries())
+		#break
+
+	#
+	#print(ranking)
+
+	print(ranking)
+	print("epoch_loss: ", overall_loss / data.train.num_queries())
 
 	# TODO: Go over data.validation
 
 def validate_ndcg():
-    with torch.no_grad():
+	with torch.no_grad():
 
-        total_ndcg = 0
+		total_ndcg = 0
 
-        for i, qid in enumerate(np.arange(data.validation.num_queries())):
-            qd_feats = data.validation.query_feat(qid)
-            qd_labels = data.validation.query_labels(qid)
-            scores = model.forward(torch.tensor(qd_feats).float().cuda())
+		for i, qid in enumerate(np.arange(data.validation.num_queries())):
+			qd_feats = data.validation.query_feat(qid)
+			qd_labels = data.validation.query_labels(qid)
+			scores = model.forward(torch.tensor(qd_feats).float().cuda())
 
-            # Make em torchy?
-            qd_feats = torch.tensor(qd_feats).cuda()
-            qd_labels = torch.tensor(qd_labels).cuda()
+			# Make em torchy?
+			qd_feats = torch.tensor(qd_feats).cuda()
+			qd_labels = torch.tensor(qd_labels).cuda()
 
-            # TODO: Fix this spaghetti shit
-            ranking_boi = np.float64(scores.squeeze().cpu().detach().numpy())
-            
-            # Get the ranking 
-            if not isinstance(ranking_boi, np.ndarray):
-                ranking_boi = np.array([ranking_boi])
-            ranking, inv_ranking = rnk.rank_and_invert(ranking_boi)
+			# TODO: Fix this spaghetti shit
+			ranking_boi = np.float64(scores.squeeze().cpu().detach().numpy())
+			
+			# Get the ranking 
+			if not isinstance(ranking_boi, np.ndarray):
+				ranking_boi = np.array([ranking_boi])
+			ranking, inv_ranking = rnk.rank_and_invert(ranking_boi)
 
-            ranking = torch.tensor(ranking.copy()).cuda()
-        
-            # Get rid of pesky 1-document queries and initialize loss
-            if len(scores) < 2:
-                continue
+			ranking = torch.tensor(ranking.copy()).cuda()
+		
+			# Get rid of pesky 1-document queries and initialize loss
+			if len(scores) < 2:
+				continue
 
-            total_ndcg += evaluate_model(model, data.validation,regression=True)
+			total_ndcg += evaluate_model(model, data.validation,regression=True)
 
-        return total_ndcg / data.validation.num_queries()
+		return total_ndcg / data.validation.num_queries()
 
 def err(ranking):
-    p = np.ones(ranking.shape[0])
-    err = 0
-    max_score = np.max(ranking)
-    for r in range(ranking.shape[1]):
-        rel_prob = (2 ** ranking[:,r] - 1) / 2 ** max_score
-        err = err + p * rel_prob/(r+1)
-        p *= (1 - rel_prob)
-    return err
+	p = np.ones(ranking.shape[0])
+	err = 0
+	max_score = np.max(ranking)
+	for r in range(ranking.shape[1]):
+		rel_prob = (2 ** ranking[:,r] - 1) / 2 ** max_score
+		err = err + p * rel_prob/(r+1)
+		p *= (1 - rel_prob)
+	return err
+
+def ndcg(ranking,ideal_ranking):
+	k = ranking.shape[1]
+	denom = 1./np.log2(np.arange(k)+2.)
+	# denom = denom.reshape(-1,1)
+	nom = 2 ** ranking-1.
+	dcg = np.sum(nom * denom, axis=1)
+	ideal_dcg_nom = 2 ** ideal_ranking-1.
+	ideal_dcg = np.sum(ideal_dcg_nom * denom)
+
+	return dcg / ideal_dcg
 
 if __name__ == "__main__":
 
@@ -187,4 +200,7 @@ if __name__ == "__main__":
 	num_epochs = 100
 	for i in range(num_epochs):
 		print("Epoch: ", i)
-		run_epoch(model, optimizer, data)
+		start = time.now()
+		run_epoch(model, optimizer, data,IRM='ndcg')
+		print("I'm done! This run lasted: " + str(time.now() - start))
+		break
