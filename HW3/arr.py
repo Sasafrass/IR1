7f9a64d62d52c0f37742594f070f9fc3d7f9229a
Aunel <christiaan_vlist@hotmail.com>
2020-03-22 21:08:51 +0100

    Fixed arr.py

--- HW3/arr.py
+++ HW3/arr.py
@@ -1,15 +1,18 @@
 from evaluate import evaluate_query
 from dataset import get_dataset, DataSet
 from models import RankNet
+import torch
+import numpy as np
 
 def calc_arr(model,datasplit):
     rr = 0
+    scores = model.forward(torch.tensor(datasplit.feature_matrix).float().cuda()).squeeze().detach().cpu().numpy()
     for i, qid in enumerate(np.arange(datasplit.num_queries())):
-        qd_feats = datasplit.query_feat(qid)
-        qd_labels = datasplit.query_labels(qid)
-        scores = model.forward(torch.tensor(qd_feats).float().cuda())
-        results = evaluate_query(datasplit,qid,scores)
-        rr += results['relevant rank per query']
+        q_labels = datasplit.query_labels(qid)
+        if(np.sum(q_labels) > 0):
+            results = evaluate_query(datasplit,qid,scores)
+            if('relevant rank' in results.keys()):
+                rr += np.mean(results['relevant rank'])
     arr = rr/i
     return arr
 
@@ -21,19 +24,20 @@ if __name__ == "__main__":
     print(data.num_features)
     data.read_data()
 
-    valitdate = True
+    validate = True
     if(validate):
-        datasplit = data.valitdation
+        datasplit = data.validation
     else:
         datasplit = data.test
 
     # Parameters for model
-	input_size  = data.num_features
-	output_size = 1					# Set this to 1 for regression, 5 for classification
-    
-    model_path = 'stored_models/pairwise_model.pth'
+    input_size  = data.num_features
+    output_size = 1					# Set this to 1 for regression, 5 for classification
+
+    model_path = 'stored_models/pairwise_model.pthlr0.0002notspedupbad'
     model = RankNet(input_size = input_size, output_size = output_size).float().cuda() # Change the model class accordingly
     model.load_state_dict(torch.load(model_path))
+    model.eval()
 
     arr = calc_arr(model,datasplit)
     print(arr)
